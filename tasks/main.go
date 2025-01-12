7e280b339516804094dd7161542fde32fc4d25c9
golnar-boosty <golnar@boostylabs.com>
2025-01-12 19:46:53 +0000

    refactor: remove unused background tasks and optimize design for better performance
    Removed background tasks that were no longer in use to streamline the application. Updated the design to improve overall system performance and reduce resource consumption.

--- tasks/main.go
+++ tasks/main.go
@@ -6,14 +6,11 @@ import (
 
 // RunTasks is a function to run threads.
 func RunTasks(gwCosmosmux *runtime.ServeMux, rpcAddr string, gatewayAddr string) {
-	go SyncStatus(rpcAddr, false)
-	go CacheHeaderCheck(rpcAddr, false)
-	go CacheDataCheck(rpcAddr, false)
-	go CacheMaxSizeCheck(false)
-	go DataReferenceCheck(false)
-	go NodeDiscover(rpcAddr, false)
-	go SyncValidators(gwCosmosmux, gatewayAddr, false)
-	go SyncProposals(gwCosmosmux, gatewayAddr, rpcAddr, false)
-	go CalcSnapshotChecksum(false)
+	go SyncStatus(rpcAddr)
+	go DataReferenceCheck()
+	go NodeDiscover(rpcAddr)
+	go SyncValidators(gwCosmosmux, gatewayAddr)
+	go SyncProposals(gwCosmosmux, gatewayAddr, rpcAddr)
+	go CalcSnapshotChecksum()
 	go SyncBitcoinWallets()
 }
