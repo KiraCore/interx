31f86698a76ea051d0c4969551f3b80d17226fda
mrlutik <yaievgeniy@gmail.com>
2025-11-18 19:20:42 +0100

    fix: interface{} -> *types.ProposalsResponse

--- manager/gateway/cosmos_agregated4.go
+++ manager/gateway/cosmos_agregated4.go
@@ -197,7 +197,7 @@ func (g *CosmosGateway) proposalsCount() (int, error) {
 	return totalCount, nil
 }
 
-func (g *CosmosGateway) getProposals(req types.InboundRequest) (interface{}, error) {
+func (g *CosmosGateway) getProposals(req types.InboundRequest) (*types.ProposalsResponse, error) {
 	proposals := new(types.ProposalsResponse)
 	limit := sekaitypes.PageIterationLimit - 1
 	offset := 0
